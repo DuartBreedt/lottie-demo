ffe7939424d153ad1884d6f2440d878d6d14dea6
Duart Breedt <duartbreedt@gmail.com>
2019-03-10 17:54:45 +0200

    Add resources + website

---
+++ lottie-demo/public/javascripts/index.js
@@ -0,0 +1,7 @@
+lottie.loadAnimation({
+    container: document.getElementById("entelect-logo"),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '../json/entelect-logo.json'
+});
